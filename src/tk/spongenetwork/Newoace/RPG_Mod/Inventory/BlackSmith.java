41beb596491e00f215728695966dfa1e37c90895
BuildTools <unconfigured@null.spigotmc.org>
2017-06-17 00:00:16 -0600

    Testing automation

--- src/tk/spongenetwork/Newoace/RPG_Mod/Inventory/BlackSmith.java
+++ src/tk/spongenetwork/Newoace/RPG_Mod/Inventory/BlackSmith.java
@@ -14,7 +14,66 @@ public class BlackSmith implements Listener {
                 return;
             } else if (e.getInventory().getTitle().equalsIgnoreCase("Anvil")) {
                 if (false) {
-
+                    /*
+                    f
+                    asd
+                    f
+                    asd
+                    fa
+                    sdf
+                    as
+                    df
+                    asd
+                    fa
+                    sdf
+                    as
+                    df
+                    asd
+                    fa
+                    sdf
+                    as
+                    df
+                    asd
+                    fa
+                    sdf
+                    as
+                    df
+                    asdf
+                    as
+                    df
+                    asd
+                    fa
+                    sdf
+                    asd
+                    fa
+                    sdf
+                    asd
+                    f
+                    asdf
+                    asd
+                    f
+                    asdf
+                    as
+                    df
+                    asd
+                    fa
+                    sdf
+                    asd
+                    fa
+                    sdf
+                    asd
+                    f
+                    asdf
+                    asdf
+                    asdf
+                    as
+                    df
+                    asd
+                    fas
+                    df
+                    asdf
+                    asdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+                     */
                 }
 
             }
